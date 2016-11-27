da6b0d74bcca192c85ba5684cb1577ca978e2dce
dnahol <dalia.nahol@gmail.com>
2016-11-27 04:43:34 -0800

    first

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+$(document).ready(function(){
+  $('.slider').slider({full_width: true});
+
+
+// Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+
+$('.carousel.carousel-slider').carousel({full_width: true});
+
+});
